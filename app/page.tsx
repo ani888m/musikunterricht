2778c57ff4f88376da8e6917a234ff55934ce428
ani888m <antonet888@yahoo.de>
2026-02-11 17:19:37 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">
+        Musikschule Plattform 🎵
+      </h1>
+
+      <p className="mt-4">
+        Willkommen auf unserer Buchungsplattform.
+      </p>
+    </main>
+  );
+}
